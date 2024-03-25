16d902b9f03133a0599bf87296252531fe6ebcf4
SusanaCajavilca <200553998@student.georgianc.on.ca>
2024-03-25 18:42:59 -0400

    added 3rd flavour

--- js/json.js
+++ js/json.js
@@ -37,21 +37,44 @@ function populateHeader(jsonData){
    header.appendChild(h1); 
 }
 /* STEP 10b: Assemble the showTopFlavors() function */
-//showTopFlavors(jsonData){}
-// STEP 10c: Bind the JSON topFlavors object to a var
+function showTopFlavors(jsonData){
 
+// STEP 10c: Bind the JSON topFlavors object to a var
+const topFlavors = jsonData.topFlavors;
 
 // STEP 10d: Loop through the topFlavors object
+   for (let i = 0; i < topFlavors.length; i++) {
+   console.log(topFlavors[i]);
 
 // STEP 10e: build HTML elements for the content: article, h2, image, p1, p2, list
-
+   let article = document.createElement("article"); //<article></article>
+   let h2 = document.createElement("h2"); //<h2></h2>
+   let p1 = document.createElement("p"); //<p></p>
+   let p2 = document.createElement("p"); //<p></p>
+   let image = document.createElement("img"); //<img>
+   let list = document.createElement("ul"); //<ul></ul>
 // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
-
+   h2.textContent = topFlavors[i].name;  
+   p1.textContent = "Calories: " + topFlavors[i].calories;  
+   p2.textContent = "Type: " + topFlavors[i].type;  
+   image.setAttribute("src",topFlavors[i].image);
 // STEP 10g: Build a loop for the ingredients array in the JSON
-
+   let ingredients = topFlavors[i].ingredients;
+   for (let j = 0; j < ingredients.length; j++) {
+      console.log(ingredients[j]);
+      let listItem = document.createElement("li");
+      listItem.textContent = ingredients[j];
+      list.appendChild(listItem);
+   }
 // STEP 10i: Append each complete ARTICLE element to the SECTION element
-
-
+   article.appendChild(h2);
+   article.appendChild(p1);
+   article.appendChild(p2);
+   article.appendChild(list);
+   article.appendChild(image);
+   section.appendChild(article);
+   }
+}
 // STEP 11: Add a 3rd flavour of ice cream to the local JSON file, making use of the /images/strawberry-sprinkle.svg image
 
 // Lab: Extend the JavaScript application built in class to include two more flavors of ice cream.
